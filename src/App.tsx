b5bff8d7cf73e8dfe34af74da75205d28b766752
Krishna <krishna@sapaad.com>
2023-08-16 14:40:57 +0530

    portfolio updates

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,21 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
-
+import Navigation from "./components/Navigation";
+import Header from "./components/Header";
+import About from "./components/About";
+import Projects from "./components/Projects";
+import Experience from "./components/Experience";
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+	return (
+		<div className="App">
+			<Navigation />
+			<Header />
+			<About />
+			<Experience />
+			<Projects />
+		</div>
+	);
 }
 
 export default App;
